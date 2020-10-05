501e321d83fe482f4155f08632d4cc746f4b0883
kinkidycodes <51934846+kinkidycodes@users.noreply.github.com>
2020-10-05 13:46:08 +0100

    Chore: Creating a servie to get album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+import { AlbumStorePage } from '../../e2e/app.po';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: '../assets/album.json';
+  // album: any;
+
+  constructor(private _http: HttpClient) { }
+
+  getAlbum( id: number): Observable<album> {
+    return this._http.get(this._albumUrl)
+      .pipe(map((response: any) => <album>response.json()));
+  }
+
+  // getAlbum(id: number): Observable<Album> {
+  //   return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  // }
+
+}
